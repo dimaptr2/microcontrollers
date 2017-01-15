f9454c98e0d19455381c73e63c2e5b5eaa0f2b74
Petrov Dmitry <petrovdmitry@iMac-Petrov.local>
2017-01-15 11:26:57 +0300

    Beaglebone service

---
+++ beaglesrv/src/main/java/ru/velkomfood/prod/info/beagle/Launcher.java
@@ -0,0 +1,14 @@
+package ru.velkomfood.prod.info.beagle;
+
+/**
+ * Created by petrovdmitry on 14.01.17.
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        System.out.println("Start the data processing ...");
+
+    }
+
+}
---
+++ beaglesrv/src/main/java/ru/velkomfood/prod/info/beagle/model/Event.java
@@ -0,0 +1,10 @@
+package ru.velkomfood.prod.info.beagle.model;
+
+/**
+ * Created by petrovdmitry on 14.01.17.
+ */
+public class Event {
+
+    private Long id;
+
+}
